6315bd39f32020f0fba12256761941772ee3a168
Olga RG <olga_1847@hotmail.com>
2022-07-06 09:35:33 +0200

    Add basic JS

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
 'use strict';
 
+const inputNumber = document.querySelector('.js_input');
+const btn = document.querySelector('.js_button');
+const clue = document.querySelector('.js_clue');
+const attempts = document.querySelector('.js_attempts');
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+};
+
+const compareNumber = () => {
+    const randomNumber = getRandomNumber(100);
+    const inputValue = inputNumber.value;
+    if(inputValue > 100 || inputValue < 1){
+        clue.innerHTML = "El número debe estar entre 1 y 100.";
+    }
+    else if(inputValue > randomNumber){
+        clue.innerHTML = "Demasiado alto.";
+    } else if(inputValue < randomNumber){
+        clue.innerHTML = "Demasiado bajo.";
+    } else {
+        clue.innerHTML = "Has ganado campeona!!!";
+    }
+    console.log(randomNumber);
+}
+
+
+function handleClick(e) {
+    e.preventDefault();
+    compareNumber();
+}
+
+
+btn.addEventListener('click', handleClick);
